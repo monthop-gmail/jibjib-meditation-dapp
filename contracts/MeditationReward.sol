91c688ea90aaacc0e02ba29cbcecc29f4e8c0b05
Monthop <monthop@gmail.com>
2026-02-21 07:23:57 +0000

    Add per-token reward amounts: support different rewards for JIBJIB, JIBJIB C, native

--- contracts/MeditationReward.sol
+++ contracts/MeditationReward.sol
@@ -5,6 +5,7 @@ interface IERC20 {
     function transfer(address to, uint256 amount) external returns (bool);
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
     function balanceOf(address account) external view returns (uint256);
+    function decimals() external view returns (uint8);
 }
 
 contract MeditationReward {
@@ -12,7 +13,6 @@ contract MeditationReward {
     string public symbol = "JIBJIB";
     uint8 public decimals = 18;
 
-    uint256 public constant REWARD_AMOUNT = 1 * 10**18;
     uint256 public constant MEDITATION_DURATION = 300 seconds;
 
     mapping(address => uint256) public lastMeditationTime;
@@ -20,16 +20,19 @@ contract MeditationReward {
     mapping(address => uint256) public totalMeditationSessions;
 
     address public owner;
-    address public rewardToken;
+    address[] public supportedTokens;
+    mapping(address => uint256) public rewardAmounts;
+    mapping(address => bool) public isTokenSupported;
     
     mapping(address => uint256) public tokenBalances;
     uint256 public nativeBalance;
 
     event MeditationStarted(address indexed user, uint256 timestamp);
-    event MeditationCompleted(address indexed user, uint256 reward);
-    event RewardClaimed(address indexed user, uint256 amount);
+    event MeditationCompleted(address indexed user, uint256 reward, address token);
+    event RewardClaimed(address indexed user, uint256 amount, address token);
     event Donated(address indexed donor, address token, uint256 amount);
     event Withdrawn(address indexed owner, address token, uint256 amount);
+    event TokenRewardSet(address indexed token, uint256 amount);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner");
@@ -38,6 +41,8 @@ contract MeditationReward {
 
     constructor() {
         owner = msg.sender;
+        supportedTokens.push(address(0));
+        isTokenSupported[address(0)] = true;
     }
 
     function startMeditation() external {
@@ -46,26 +51,44 @@ contract MeditationReward {
         emit MeditationStarted(msg.sender, block.timestamp);
     }
 
-    function completeMeditation() external {
+    function completeMeditation(address token) external {
         require(lastMeditationTime[msg.sender] > 0, "No active meditation");
         require(
             block.timestamp - lastMeditationTime[msg.sender] >= MEDITATION_DURATION,
             "Meditation not complete"
         );
+        require(isTokenSupported[token], "Token not supported");
+        
+        uint256 reward = rewardAmounts[token];
+        require(reward > 0, "Reward not set for this token");
 
         lastMeditationTime[msg.sender] = 0;
         totalMeditationSessions[msg.sender]++;
 
-        emit MeditationCompleted(msg.sender, REWARD_AMOUNT);
-        emit RewardClaimed(msg.sender, REWARD_AMOUNT);
+        if (token == address(0)) {
+            require(nativeBalance >= reward, "Insufficient native balance");
+            nativeBalance -= reward;
+            payable(msg.sender).transfer(reward);
+        } else {
+            require(tokenBalances[token] >= reward, "Insufficient token balance");
+            tokenBalances[token] -= reward;
+            IERC20(token).transfer(msg.sender, reward);
+        }
+
+        emit MeditationCompleted(msg.sender, reward, token);
+        emit RewardClaimed(msg.sender, reward, token);
     }
 
-    function getRewardAmount() external pure returns (uint256) {
-        return REWARD_AMOUNT;
+    function getRewardAmount(address token) external view returns (uint256) {
+        return rewardAmounts[token];
     }
 
     function getMeditationDuration() external pure returns (uint256) {
         return MEDITATION_DURATION;
+    }
+
+    function getSupportedTokens() external view returns (address[] memory) {
+        return supportedTokens;
     }
 
     function getUserStats(address user) external view returns (
@@ -80,8 +103,14 @@ contract MeditationReward {
         );
     }
 
-    function setRewardToken(address _token) external onlyOwner {
-        rewardToken = _token;
+    function setRewardAmount(address token, uint256 amount) external onlyOwner {
+        require(amount > 0, "Invalid amount");
+        rewardAmounts[token] = amount;
+        if (!isTokenSupported[token]) {
+            supportedTokens.push(token);
+            isTokenSupported[token] = true;
+        }
+        emit TokenRewardSet(token, amount);
     }
 
     function donate(address token, uint256 amount) external payable {
